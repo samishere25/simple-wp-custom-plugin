eb9ea7e371fdd320cc710c2cda4bd0b78539dbba
samiksha channawar <samikshac1001@gmail.com>
2026-01-18 23:57:44 +0530

    Add admin notice using WordPress hook

--- simple-wp-custom-plugin.php
+++ simple-wp-custom-plugin.php
@@ -6,3 +6,12 @@ Version: 1.0
 Author: Samishere25
 */
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+add_action( 'admin_notices', 'swcp_show_admin_notice' );
+
+function swcp_show_admin_notice() {
+    echo '<div class="notice notice-success"><p>Custom admin notice loaded.</p></div>';
+}
