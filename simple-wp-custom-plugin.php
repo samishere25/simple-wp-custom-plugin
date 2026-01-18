881fdf83e2b80fabe05dda7f8d98ea1e9e23b938
samiksha channawar <samikshac1001@gmail.com>
2026-01-18 23:56:37 +0530

    Initial plugin structure

--- simple-wp-custom-plugin.php
+++ simple-wp-custom-plugin.php
@@ -1,21 +1,8 @@
 <?php
 /*
 Plugin Name: Simple WP Custom Plugin
-Description: Demonstrates basic WordPress hooks and input sanitization.
+Description: Demonstrates basic WordPress hooks.
 Version: 1.0
 Author: Samishere25
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-// Display a custom admin notice
-add_action( 'admin_notices', 'swcp_show_admin_notice' );
-
-function swcp_show_admin_notice() {
-    if ( isset( $_GET['swcp_message'] ) ) {
-        $message = sanitize_text_field( $_GET['swcp_message'] );
-        echo '<div class="notice notice-success is-dismissible"><p>' . esc_html( $message ) . '</p></div>';
-    }
-}
